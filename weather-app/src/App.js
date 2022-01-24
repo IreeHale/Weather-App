0b00c68ff7678835b274b4d68caecb02639690aa
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2022-01-23 20:58:41 -0500

    adding jsx and css styles

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState }from 'react';
 
 function App() {
-  return <div>
-    <h1>Hello there</h1>
-  </div>;
+
+const apiKey = '3162758ae232875e7449531149e65a5e'
+const {weatherData, setWeatherData} = useState([{}])
+  return <div className="container">
+    <input className="input" placeholder="Enter City..." />
+  </div>
 }
 
 export default App;
