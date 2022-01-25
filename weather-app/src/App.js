d9bafd66c9bd464ea347b5bb80354f25f1b84349
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2022-01-24 21:22:58 -0500

    redo files and fixing bugs

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -5,7 +5,9 @@ const apiKey = '3162758ae232875e7449531149e65a5e'
 const [weatherData, setWEatherData] = useState([{}])
 
 function App() {
-  return <div className="container"></div>;
+  return <div className="container">
+    <input className="input" placeholder="Enter city..."/>
+  </div>
 }
 
 export default App;
