286f7d2518e8b0f20a54f750934bfbc0ee59bc63
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2022-01-25 00:35:41 -0500

    fixing typos and bugs in code project

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,14 +1,48 @@
 import React, {useState} from 'react'
 import './App.css'
 
+function App() { 
 const apiKey = '3162758ae232875e7449531149e65a5e'
-const [weatherData, setWEatherData] = useState([{}])
+const [weatherData, setWeatherData] = useState([{}])
+const [city, setCity] = useState("")
 
-function App() {
-  return <div className="container">
-    <input className="input" placeholder="Enter city..."/>
-  </div>
+const getWeather = (event) => {
+  if(event.key === "Enter") {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}`).then(
+      response => response.json()
+    ).then(
+      data => {
+        setWeatherData(data)
+      }
+    )
+  }
+ }
+
+
+  return (
+    <div className="container">
+      <input className="input" 
+      placeholder="Enter city..."
+      onChange={e => setCity(e.target.value)}
+      value={city}
+      onKeyPress={getWeather}
+      />
+
+      {typeof weatherData.main === 'undefined' ? (
+        <div>
+          <p>Welcome to weather app! Enter in a city to get the weather of.</p>
+        </div>
+      ): (
+        <div>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+      )}
+
+
+    </div>
+
+  )
 }
-
-export default App;
-
+export default App
